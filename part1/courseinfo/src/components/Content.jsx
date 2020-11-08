dc87cc375b4927162f93fc4084db39cf8bdee8d4
Abdulsamii Ajala <kgasta@gmail.com>
2020-11-08 04:32:56 +0100

    feat: ex1 step2

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
+function Part ({ part, exercise }) {
+  return (<p>
+    {part} {exercise}
+  </p>)
+}
+
 function Content({ part1, exercises1, part2, exercises2, part3, exercises3 }) {
   return (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercise={exercises1}  />
+      <Part part={part2} exercise={exercises2}  />
+      <Part part={part3} exercise={exercises3}  />
     </>
   )
 }
