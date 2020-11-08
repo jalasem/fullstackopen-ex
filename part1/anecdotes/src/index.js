389d4e15c58d0de420ea9e64c601f714d1a00f78
Abdulsamii Ajala <kgasta@gmail.com>
2020-11-08 10:39:39 +0100

    feat: anecdotes step3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from "react-dom";
 const App = (props) => {
   const [selected, setSelected] = useState(0);
   const [points, setPoints] = useState(new Array(6).fill(0));
+  const [highest, setHighest] = useState({
+    value: 0,
+    index: 0
+  })
 
   const nextAnecdote = () =>
     setSelected(Math.floor(Math.random() * anecdotes.length));
@@ -11,16 +15,24 @@ const App = (props) => {
   const vote = () => {
     setPoints(pts => pts.map((pt, index) => {
       if (index === selected) pt += 1
+      if (pt > highest.value) setHighest({ value: pt, index })
       return pt
     }))
   }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{props.anecdotes[selected]}</p>
       <p>has {points[selected]} votes</p>
       <button onClick={() => vote()}>vote</button>
       <button onClick={() => nextAnecdote()}>next anecdote</button>
+      
+      {Boolean(highest.value) && (<>
+        <h1>Anecdote with most votes</h1>
+        <p>{props.anecdotes[highest.index]}</p>
+        <p>has {highest.value} votes</p>
+      </>)}
     </div>
   );
 };
