49b8b8913989ab0723feda57fe688f18a2244b84
Abdulsamii Ajala <kgasta@gmail.com>
2020-11-08 21:43:21 +0100

    fix: code cleanup

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,12 +5,7 @@ import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    // { name: 'Arto Hellas', number: '040-123456' },
-    // { name: 'Ada Lovelace', number: '39-44-5323523' },
-    // { name: 'Dan Abramov', number: '12-43-234345' },
-    // { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [filter, setFilter] = useState('')
@@ -18,16 +13,12 @@ const App = () => {
   const handleSubmit = e => {
     e.preventDefault()
 
-    if (!newName || newNumber) return 
+    if (!newName || !newNumber) return 
 
     if (persons.find(person => person.name === newName)) {
       alert(`${newName} is already added to the numberbook`)
       return
     }
-    // else if (persons.find(person => person.number === newnumber)) {
-    //   alert(`${newnumber} is already added to the numberbook`)
-    //   return
-    // }
 
     setPersons(persons => persons.concat({ name: newName, number: newNumber }))
     setNewName('')
