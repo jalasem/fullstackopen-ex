2074506651f35f9c2d286c871c8d8902443d18b0
Abdulsamii Ajala <kgasta@gmail.com>
2020-11-09 12:53:46 +0100

    feat: Data for countries, step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,14 +1,43 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const CountryInfo = ({ country }) => (<div>
+  <h2>{country.name}</h2>
+  <p>Capital {country.capital}</p>
+  <p>Population {country.population.toLocaleString()}</p>
+  <h3>Languages</h3>
+  <ul>
+    {country.languages.map((lang, langIndex) => (
+      <li key={`${country.name}_lang-${langIndex}`}>
+        {lang.name}
+      </li>
+    ))}
+  </ul>
+  <img src={country.flag} alt={country.name} width="100" />
+</div>)
+
 function App() {
   const [query, setQuery] = useState("");
   const [countries, setCountries] = useState([]);
 
+  const showCountry = (alpha3Code) => {
+    setCountries(countries => countries.map(country => {
+      if (country.alpha3Code === alpha3Code) country.shown = true
+      return country
+    }))
+  }
+
   useEffect(() => {
     if (query.trim().length) {
       axios(`https://restcountries.eu/rest/v2/name/${query}`)
-        .then(({ data }) => setCountries(data))
+        .then(({ data }) =>
+          setCountries(
+            data.map((country) => {
+              country.shown = false;
+              return country;
+            })
+          )
+        )
         .catch((err) => console.error({ err }));
     }
   }, [query]);
@@ -22,22 +51,15 @@ function App() {
       {countries.length > 10 ? (
         <p>Too many matches, specify another filter</p>
       ) : countries.length === 1 ? (
-        <div>
-          <h2>{countries[0].name}</h2>
-          <p>Capital {countries[0].capital}</p>
-          <p>Population {countries[0].population.toLocaleString()}</p>
-          <h3>Languages</h3>
-          <ul>
-            {countries[0].languages.map((lang, langIndex) => (
-              <li key={`${countries[0].name}_lang-${langIndex}`}>{lang.name}</li>
-            ))}
-          </ul>
-          <img src={countries[0].flag} alt={countries[0].name} width="100" />
-        </div>
+        <CountryInfo country={countries[0]} />
       ) : (
-        countries.map((country) => (
-          <p key={country.alpha3Code}>{country.name}</p>
-        ))
+        countries.map((country) =>
+          country.shown ? (
+            <CountryInfo country={country} />
+          ) : (
+            <p key={country.alpha3Code}>{country.name} <button onClick={() => showCountry(country.alpha3Code)}>Show</button></p>
+          )
+        )
       )}
     </div>
   );
